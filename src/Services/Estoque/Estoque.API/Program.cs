0bb0559ebc4e60ac25800667ed61e5868a10adf4
AlansidneyJunior <alansidney.silva21@gmail.com>
2025-11-12 20:38:29 -0300

    Configurando Infraestrutura de Estoque

--- src/Services/Estoque/Estoque.API/Program.cs
+++ src/Services/Estoque/Estoque.API/Program.cs
@@ -1,7 +1,19 @@
+using Estoque.Domain.Interfaces;
+using Estoque.Infrastructure.Data;
+using Estoque.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Adicionar Controllers
 builder.Services.AddControllers();
+
+// Configurar DbContext com PostgreSQL
+builder.Services.AddDbContext<EstoqueDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Registrar Repositórios
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 
 var app = builder.Build();
 
